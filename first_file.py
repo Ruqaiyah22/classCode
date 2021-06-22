9d4636f7846140cb6055db80c3fce3a0ecfa37d8
“Ruqaiyah <kaya4152201@gmail.com>
2021-06-22 16:46:35 -0700

    lab 2 creating and printing out types of variables

--- first_file.py
+++ first_file.py
@@ -1,1 +1,23 @@
-print("Hello World! :)")
+print("")
+print("Hello World! :P")
+print("")
+
+#STEP 1
+name = "LeBron" #dataType: string
+print(type(name))
+age = 35 #dataType: int
+print(type(age))
+salary = 37.44 # dataType: float
+print(type(salary))
+plays_basketball = True #dataType: boolean
+print(type(plays_basketball))
+jersey_number = "23" #dataType: string
+print(type(jersey_number))
+
+#STEP 2
+car_name = "Tesla"
+print(type(car_name))
+x = 50
+print(type(x))
+my_first_name = "John"
+print(type(my_first_name))
